27d08826cbda616f87d56f004dda2004ea59773e
Alvaro Dorado <alvaro.dorado@babelgroup.com>
2023-02-24 13:51:01 +0100

    Actualizacion AlvaroD

---
+++ TiendaSolid/src/main/java/Consola.java
@@ -0,0 +1,9 @@
+public class Consola extends Producto{
+
+    private int idConsola;
+
+    public Consola(String nombreProducto, int stock, String descripcion, double precio) {
+        super(nombreProducto, stock, descripcion, precio);
+    }
+
+}
