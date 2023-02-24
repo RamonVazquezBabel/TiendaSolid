643113b2474b00ab5594c740ee489e430562314e
RafaVera <rafael.vera@babelgroup.com>
2023-02-24 13:44:06 +0100

    Creando Clase Movil

---
+++ TiendaSolid/src/main/java/Movil.java
@@ -0,0 +1,17 @@
+package src.main.java;
+
+public class Movil {
+    private int idMovil;
+
+    Public Movil(int idMovil) {
+        this.idMovil = idMovil;
+    }
+
+    public int getIdMovil() {
+        return idMovil;
+    }
+
+    public void setIdMovil(int idMovil) {
+        this.idMovil = idMovil;
+    }
+}
