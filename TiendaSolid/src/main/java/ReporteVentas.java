2c44aa7001aff93f0416e6d973bc1c88d41752c4
RafaVera <rafael.vera@babelgroup.com>
2023-02-27 09:39:47 +0100

    Clase ReporteVentas

---
+++ TiendaSolid/src/main/java/ReporteVentas.java
@@ -0,0 +1,23 @@
+package src.main.java;
+
+import java.util.List;
+
+public class ReporteVentas {
+    private List<Venta> ventas;
+
+    public ReporteVentas(List<Venta> ventas) {
+        this.ventas = ventas;
+    }
+
+    public int getCantidadVentas() {
+        return ventas.size();
+    }
+
+    public double getMontoTotalVentas() {
+        return ventas.stream().mapToDouble(this::calcularMontoVenta).sum();
+    }
+
+    private double calcularMontoVenta(Venta venta) {
+        return venta.getProductos().stream().mapToDouble(Producto::getPrecio).sum();
+    }
+}
